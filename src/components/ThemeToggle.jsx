fdeb486db3c79dc2696d04efed5a54197a60b6ce
Facundo177 <facundogaitansegura@gmail.com>
2026-06-15 18:10:50 -0300

    Corrección al botón de tema claro/oscuro, cambio de posición y color

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -30,7 +30,7 @@ export const ThemeToggle = () => {
 
   return (
     <button onClick={toggleTheme} className={cn(
-      "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full",
+      "fixed bottom-5 right-5 z-50 p-2 rounded-full bg-secondary/50",
       "transition-colors duration-300 focus:outlin-hidden"
     )}>
       {isDarkMode
