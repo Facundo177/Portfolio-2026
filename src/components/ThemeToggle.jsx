666571b3e9f65aa1a1785c2f9190974042bb6f87
Facundo177 <facundogaitansegura@gmail.com>
2026-06-21 01:39:42 -0300

    Tema oscuro es el nuevo default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -6,12 +6,12 @@ export const ThemeToggle = ({ isDarkMode, setIsDarkMode }) => {
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
-    } else {
+    if (storedTheme === "light") {
       localStorage.setItem("theme", "light");
       setIsDarkMode(false);
+    } else {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
     }
   }, []);
 
